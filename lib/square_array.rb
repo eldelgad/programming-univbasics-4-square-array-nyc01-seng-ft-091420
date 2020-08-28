3b4fcd4b1b20273fd7447204e24935ee466535a5
Elias Delgado <evdelgado99@gmail.com>
2020-08-28 19:34:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length()
-    new_array.push(array[counter] ** array[counter])
+    new_array.push(array[counter] * array[counter])
     counter += 1
   end 
   
